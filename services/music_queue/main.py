9bec447034fdabdb2a9645045f745ebb6c38d309
Jonathan Neves <jonathannalencar@gmail.com>
2025-01-20 02:36:58 -0300

    unenecessary prints removed

--- services/music_queue/main.py
+++ services/music_queue/main.py
@@ -38,9 +38,7 @@ def current_song():
 
 @app.route('/delete', methods=['POST'])
 def delete_song():
-    print(f"Request: {request.json}")
     pos = request.json.get('pos')
-    print(f"Tentou deletar a musica na pos: {pos}")
     db.delete_song(pos)
     return jsonify({'message': 'Musica removida!'})
 
